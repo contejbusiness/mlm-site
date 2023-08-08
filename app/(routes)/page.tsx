573aa974ed24948d7d70dfee4bf778ec16214e8b
kamal.swami <contej.business@gmail.com>
2023-08-08 21:39:35 +0530

    updated access middleware

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,5 +1,19 @@
-function HomePage() {
-  return <div>HomePage</div>;
-}
+import Billboard from "@/components/ui/billboard";
+import getBillboard from "@/actions/get-billboard";
+import Container from "@/components/ui/container";
+
+export const revalidate = 0;
+
+const HomePage = async () => {
+  const billboard = await getBillboard("42e9db94-9932-4a33-addf-af821207c9df");
+
+  return (
+    <Container>
+      <div className="space-y-10 pb-10">
+        <Billboard data={billboard} />
+      </div>
+    </Container>
+  );
+};
 
 export default HomePage;
---
+++ components/footer.tsx
@@ -0,0 +1,13 @@
+const Footer = () => {
+  return (
+    <footer className="bg-white border-t">
+      <div className="mx-auto py-10">
+        <div className="text-center text-xs text-black">
+          &copy; 2023 Contej Store, Inc. All rights reserved
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
