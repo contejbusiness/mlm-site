b3b581a05847d0fb6e0a26c843590ff29678f412
kamal.swami <contej.business@gmail.com>
2023-12-06 20:54:48 +0530

    added shadcn

---
+++ app/(routes)/balance/page.tsx
@@ -0,0 +1,11 @@
+import Container from "@/components/ui/container";
+
+const BalancePage = () => {
+  return (
+    <Container>
+      <div>BalancePage</div>
+    </Container>
+  );
+};
+
+export default BalancePage;
