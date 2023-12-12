34fdec6a7148712d7e8f1b8f06f6a7a1b42649d7
kamal.swami <contej.business@gmail.com>
2023-12-12 19:34:45 +0530

    fixed import of plan types

--- actions/get-plans.tsx
+++ actions/get-plans.tsx
@@ -1,11 +1,9 @@
-import { Plan } from "@/types";
-
 const URL = `${process.env.NEXT_PUBLIC_API_URL}/plans`;
 
 const getPlans = async (): Promise<any[]> => {
   const res = await fetch(`${URL}`);
 
-  console.log("🚀 ~ file: get-plans.tsx:8 ~ getPlans ~ res:", res)
+  console.log("🚀 ~ file: get-plans.tsx:8 ~ getPlans ~ res:", res);
   return res.json();
 };
 
