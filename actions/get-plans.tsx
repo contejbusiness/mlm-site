f66b481d93f28ee7b2ba02cf755cf2eee58f5cff
kamal.swami <contej.business@gmail.com>
2023-12-16 21:47:05 +0530

    plans fixed

--- actions/get-plans.tsx
+++ actions/get-plans.tsx
@@ -6,7 +6,7 @@ const getPlans = async (): Promise<Plan[]> => {
   const res = await fetch(`${URL}`);
 
   console.log(URL);
-  console.log("🚀 ~ file: get-plans.tsx:6 ~ getPlans ~ res:", await res.json());
+
   return res.json();
 };
 
