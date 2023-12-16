b1c5f34c6cf98e784fb606fab600da9d92ab5e69
kamal.swami <contej.business@gmail.com>
2023-12-16 13:46:59 +0530

    bug fixes

--- actions/get-user.tsx
+++ actions/get-user.tsx
@@ -1,7 +1,7 @@
 import { User } from "@/types";
 import { currentUser, redirectToSignIn } from "@clerk/nextjs";
 
-const getCurrentUser = async (): Promise<User> => {
+const getCurrentUser = async () => {
   try {
     const user = await currentUser();
 
@@ -12,10 +12,11 @@ const getCurrentUser = async (): Promise<User> => {
     const res = await fetch(
       `${process.env.NEXT_PUBLIC_API_URL}/users?userId=${user.id}`
     );
+    console.log("🚀 ~ file: get-user.tsx:16 ~ getCurrentUser ~ res:", res);
 
     return res.json();
-  } catch (error: any) {
-    return error;
+  } catch (error) {
+    return { error: true };
   }
 };
 
--- components/navwrapper.tsx
+++ components/navwrapper.tsx
@@ -1,11 +1,20 @@
 import React from "react";
 import Navbar from "./ui/v2/navbar";
 import getCurrentUser from "@/actions/get-user";
+import { redirectToSignIn } from "@clerk/nextjs";
 
 const NavWrapper = async () => {
   const user = await getCurrentUser();
 
-  return <Navbar user={user} />;
+  if (user.error) {
+    return <h1>Failed to Get User</h1>;
+  }
+
+  return (
+    <div>
+      <Navbar user={user} />
+    </div>
+  );
 };
 
 export default NavWrapper;
