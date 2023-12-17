c11a424d5446bd7978fb4f57bdb818a45d7ea450
kamal.swami <contej.business@gmail.com>
2023-12-17 09:14:21 +0530

    revalition updated

--- actions/get-plans.tsx
+++ actions/get-plans.tsx
@@ -3,7 +3,7 @@ import { Plan } from "@/types";
 const URL = `${process.env.NEXT_PUBLIC_API_URL}/plans`;
 
 const getPlans = async (): Promise<Plan[]> => {
-  const res = await fetch(`${URL}`, { next: { revalidate: 3600 } });
+  const res = await fetch(`${URL}`, { next: { revalidate: 60 } });
 
   return res.json();
 };
